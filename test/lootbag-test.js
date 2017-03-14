e97b0b854c0c67a04ebae0ffaf61c9c9d99f0bc7
Luke Schuyler <lukeschuyler1@gmail.com>
2017-03-14 12:02:10 -0500

    first test passing, testing add function

--- test/lootbag-test.js
+++ test/lootbag-test.js
@@ -1,1 +1,12 @@
 const { assert } = require('chai')
+const { addToy } = require('../lib/lootbag.js')
+
+describe('lootbag', () => {
+	describe('addToy', () => {
+		it('should add two arguments to a list toys/names', () => {
+			let array = []
+			array.push(addToy('stapler', 'Milton'))
+			assert.deepEqual([{toy: 'stapler', kid: 'Milton'}], array)
+		})
+	})
+})
