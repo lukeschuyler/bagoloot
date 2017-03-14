5083532c11c50a94bfeb80ecd9cbcfb7209e5ddb
Luke Schuyler <lukeschuyler1@gmail.com>
2017-03-14 14:11:28 -0500

    4 tests passing on basic functionality of add/remove toy functions, sql data file initiated

--- test/lootbag-test.js
+++ test/lootbag-test.js
@@ -1,12 +1,30 @@
 const { assert } = require('chai')
-const { addToy } = require('../lib/lootbag.js')
+const { addToy, removeToy } = require('../lib/lootbag.js')
 
 describe('lootbag', () => {
 	describe('addToy', () => {
+
 		it('should add two arguments to a list toys/names', () => {
-			let array = []
-			array.push(addToy('stapler', 'Milton'))
-			assert.deepEqual([{toy: 'stapler', kid: 'Milton'}], array)
-		})
-	})
-})
+			let list = []
+			addToy('stapler', 'Milton', list)	
+			assert.deepEqual([{toy: 'stapler', kid: 'Milton'}], list)
+		});
+
+		it('should throw error if anything other than three arguments are passed', () => {
+			assert.throws(() => addtoy('hello'), Error)
+		});
+
+	});
+
+	describe('removeToy', () => {
+		it('should remove specified item from list', () => {
+			let list = [ {kid: 'Milton', toy: 'stapler'} ]
+			removeToy( 'Milton', 'stapler', list )
+			assert.deepEqual([], list)
+		});
+
+		it('should throw error if anything other than three arguments are passed', () => {
+			assert.throws(() => removeToy('hello'), Error)
+		});
+	});
+});
