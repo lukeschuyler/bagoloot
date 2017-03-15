6e216d8d296e9548408464e412ec80e86dc73ae5
Luke Schuyler <lukeschuyler1@gmail.com>
2017-03-15 10:54:33 -0500

    add and remove funcs fully tested, passing

--- test/lootbag-test.js
+++ test/lootbag-test.js
@@ -1,4 +1,4 @@
-const { assert } = require('chai')
+const { assert, expect } = require('chai')
 const { addToy, addTest } = require('../lib/addToy.js')
 const { removeToy } = require('../lib/removeToy.js')
 
@@ -12,14 +12,40 @@ describe('lootbag', () => {
 				resolve(addToy('Scooter', kid))
 			}).then(() => {
 				return addTest(kid)
-					.then((kid) => {
-						assert.isObject(kid)
-						console.log(kid)
+					.then((kid_data) => {
+						assert.isObject(kid_data)
+						console.log(kid_data)
 				})
 				.catch(() => console.log('catch'))
 			})
 		});
 
+		it('should error if two parameters are not passed', () => {		
+			assert.throws( () => { addToy('gi joe') } )
+		})
 	});
+
+	describe('removeToy', () => {
+		it('should remove a row from database based on two parameters', () => {
+			let kid = 'Johnny'
+			new Promise((resolve, reject) => {
+				resolve(removeToy('Scooter', kid))
+			}).then(() => {
+				return addTest(kid)
+					.then((kid_data) => {
+						assert.isUndefined(kid_data.Name)
+				})
+			})
+			.catch(() => {
+				assert.isUndefined(kid_data.Name)
+			})
+		});
+
+		it('should error if two parameters are not passed', () => {		
+			assert.throws( () => { removeToy('gi joe') } )
+		})
+	});
+
+	// describe('')
 
 });
