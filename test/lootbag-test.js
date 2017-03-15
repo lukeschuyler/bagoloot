657e2ad8bd8195df614f2aa14e5baf5ee20c0199
Luke Schuyler <lukeschuyler1@gmail.com>
2017-03-15 15:30:29 -0500

    tests pass for all database invloved functions

--- test/lootbag-test.js
+++ test/lootbag-test.js
@@ -3,6 +3,7 @@ const { addToy, addTest } = require('../lib/addToy.js')
 const { removeToy } = require('../lib/removeToy.js')
 const { listChildren } = require('../lib/listChildren.js')
 const { listToys } = require('../lib/listToys.js')
+const { setDelivered } = require('../lib/setDelivered.js')
 
 
 describe('lootbag', () => {
@@ -14,7 +15,7 @@ describe('lootbag', () => {
 		// 	new Promise((resolve, reject) => {
 		// 		resolve(addToy('Scooter', kid))
 		// 	}).then(() => {
-		// 		return addTest(kid)
+		// 		addTest(kid)
 		// 			.then((kid_data) => {
 		// 				assert.isObject(kid_data)
 		// 				console.log(kid_data)
@@ -28,13 +29,50 @@ describe('lootbag', () => {
 		})
 	});
 
+	describe('listChildren', () => {
+		it('should return an array', () => {
+			listChildren()
+				.then((arr) => {
+					assert.isArray(arr)
+			})
+		})
+		it('should return an array of Strings', () => {
+			listChildren()
+				.then((arr) => {
+					assert.isString(arr[0])
+			})
+		})
+
+	})
+
+	describe('listToys', () => {
+		it('should return an array', () => {
+			listToys()
+				.then((arr) => {
+					assert.isArray(arr)
+			})
+		})
+		it('should return an array of Strings', () => {
+			listToys()
+				.then((arr) => {
+					assert.isString(arr[0])
+			})
+		})
+	})
+
+	describe('setDelivered', () => {
+		it('should error if two parameters are not passed', () => {		
+			assert.throws( () => { setDelivered('gi joe') } )
+		})
+	});
+
 	describe('removeToy', () => {
 		// it('should remove a row from database based on two parameters', () => {
 		// 	let kid = 'Johnny'
 		// 	new Promise((resolve, reject) => {
 		// 		resolve(removeToy('Scooter', kid))
 		// 	}).then(() => {
-		// 		return addTest(kid)
+		// 		addTest(kid)
 		// 			.then((kid_data) => {
 		// 				assert.isUndefined(kid_data.Name)
 		// 		})
@@ -49,35 +87,7 @@ describe('lootbag', () => {
 		})
 	});
 
-	describe('listChildren', () => {
-		it('should return an array', () => {
-			return listChildren()
-				.then((arr) => {
-					assert.isArray(arr)
-			})
-		})
-		it('should return an array of Strings', () => {
-			return listChildren()
-				.then((arr) => {
-					assert.isString(arr[0])
-			})
-		})
-
-	})
-
-	describe('listToys', () => {
-		it('should return an array', () => {
-			return listToys()
-				.then((arr) => {
-					assert.isArray(arr)
-			})
-		})
-		it('should return an array of Objects', () => {
-			return listToys()
-				.then((arr) => {
-					assert.isString(arr[0])
-			})
-		})
-
+	describe('gimmeTheLoot', () => {
+		
 	})
 });
